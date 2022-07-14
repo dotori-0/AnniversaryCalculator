77b5a15bd5dd6faa9b363f2cac8dfd0adace6055
dotori-0 <sesan131@gmail.com>
2022-07-14 20:54:02 +0900

    [Chore] Test

---
+++ AnniversaryCalculator/AppDelegate.swift
@@ -0,0 +1,41 @@
+//
+//  AppDelegate.swift
+//  DDayCalculator
+//
+//  Created by SC on 2022/07/13.
+//
+
+import UIKit
+import IQKeyboardManagerSwift  // 1번
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+
+        IQKeyboardManager.shared.enable = true  // 2번
+        // 키보드 영역에 텍스트필드가 있어서 가려지는 경우, 텍스트필드 만큼의 높이를 자동으로 올려줬다가 내려 준다.
+
+        return true
+    }
+
+    // MARK: UISceneSession Lifecycle
+
+    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
+        // Called when a new scene session is being created.
+        // Use this method to select a configuration to create the new scene with.
+        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
+    }
+
+    func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
+        // Called when the user discards a scene session.
+        // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
+        // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
+    }
+
+
+}
+
